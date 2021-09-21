3e48abe9c47e1b822a9b5e56da5625fc0420dfdf
Arhaan Bahadur <arhaanb@gmail.com>
2021-09-21 16:54:53 +0530

    disable ssg for now

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import App from './App.vue'
 import { routes } from './routes.js'
-import { ViteSSG } from 'vite-ssg'
+// import { ViteSSG } from 'vite-ssg'
 import NProgress from 'nprogress'
 import './assets/css/normal.css'
 import './assets/css/skeleton.css'
@@ -8,32 +8,58 @@ import './assets/css/global.css'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
-export const createApp = ViteSSG(
-	App,
-	{
-		routes,
-		scrollBehavior(to, from, savedPosition) {
-			if (to.hash) {
-				return {
-					el: to.hash,
-					behavior: 'smooth'
-				}
-			} else {
-				return { top: 0 }
-			}
-		}
-	},
-	({ app, router, isClient }) => {
-		// @ts-ignore
-		app.use(AOS.init())
+// export const createApp = ViteSSG(
+// 	App,
+// 	{
+// 		routes,
+// 		scrollBehavior(to, from, savedPosition) {
+// 			if (to.hash) {
+// 				return {
+// 					el: to.hash,
+// 					behavior: 'smooth'
+// 				}
+// 			} else {
+// 				return { top: 0 }
+// 			}
+// 		}
+// 	},
+// 	({ app, router, isClient }) => {
+// 		// @ts-ignore
+// 		app.use(AOS.init())
 
-		if (isClient) {
-			router.beforeEach(() => {
-				NProgress.start()
-			})
-			router.afterEach(() => {
-				NProgress.done()
-			})
+// 		if (isClient) {
+// 			router.beforeEach(() => {
+// 				NProgress.start()
+// 			})
+// 			router.afterEach(() => {
+// 				NProgress.done()
+// 			})
+// 		}
+// 	}
+// )
+
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const app = createApp(App)
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes,
+	scrollBehavior(to, from, savedPosition) {
+		if (to.hash) {
+			return {
+				el: to.hash,
+				behavior: 'smooth'
+			}
+		} else {
+			return { top: 0 }
 		}
 	}
-)
+})
+
+// @ts-ignore
+app.use(AOS.init())
+
+app.use(router)
+app.mount('#app')
